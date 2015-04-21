13113bc3eadc42a1666c0c8ff54fee9c69b01f3a
NorthernSpark <joe@northernspark.co.uk>
2015-04-21 22:30:47 +0100

    Name fix
    Issue with app name returning NULL has been fixed

--- NSLogger.m
+++ NSLogger.m
@@ -9,15 +9,15 @@
 #import "NSLogger.h"
 
 #define LOGGER_DIRECTORY [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"logger"]
-#define LOGGER_APP [[[NSBundle mainBundle] localizedInfoDictionary] objectForKey:@"CFBundleDisplayName"]
+#define LOGGER_APP [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"]
 #define LOGGER_BUNDLE [[NSBundle mainBundle] bundleIdentifier]
 #define LOGGER_DEVICE_NAME [UIDevice currentDevice].name
 #define LOGGER_LANGUAGE [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:[[NSLocale currentLocale] localeIdentifier]]
 #define LOGGER_DEVICE_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
 #define LOGGER_APP_FORMATTED [[LOGGER_APP stringByReplacingOccurrencesOfString:@" " withString:@"-"] lowercaseString]
 #define LOGGER_FILENAME [NSString stringWithFormat:@"%@-logger.txt" ,LOGGER_APP_FORMATTED]
-#define LOGGER_VERSION 1.0
-#define LOGGER_HEADER [NSString stringWithFormat:@"Created with NorthernSpark Logger (Version %f)\n (c)\nLog Created: %@\nApp Name: %@\nApp Bundle Identifyer: %@\nDevice: %@ (iOS %f)\nLocalization: %@\n\n\n" ,LOGGER_VERSION, [self formatDate], LOGGER_APP, LOGGER_BUNDLE, LOGGER_DEVICE_NAME, LOGGER_DEVICE_VERSION, LOGGER_LANGUAGE]
+#define LOGGER_VERSION 1.01
+#define LOGGER_HEADER [NSString stringWithFormat:@"Created with NorthernSpark Logger (Version %.1f)\nLog Created: %@\nApp Name: %@\nApp Bundle Identifyer: %@\nDevice: %@ (iOS %.1f)\nLocalization: %@\n\n" ,LOGGER_VERSION, [self formatDate], LOGGER_APP, LOGGER_BUNDLE, LOGGER_DEVICE_NAME, LOGGER_DEVICE_VERSION, LOGGER_LANGUAGE]
 
 @implementation NSLogger
 
@@ -43,11 +43,14 @@
     
     [appendContents appendString:[self logPrint]];
     [appendContents appendString:[NSString stringWithFormat:@"\n\n****************************** %@ ******************************\n" ,[self formatDate]]];
+    
+    NSAssert(title == nil, @"Event title cannot be nil");
+    NSAssert(title.length < 3, @"Event title needs to be longer that 2 characters");
+
     [appendContents appendString:[NSString stringWithFormat:@"\nEVENT: \"%@\"\nPROPERTIES ¬ \n" ,title]];
     
     for (int i = 0; i < [[properties allKeys] count]; i++) {
-        if ([[properties objectForKey:[[properties allKeys] objectAtIndex:i]] isKindOfClass:[NSNumber class]]) NSAssert(1, @"properties dictionary cannot contain NSNumber objects.");
-        else [appendContents appendString:[NSString stringWithFormat:@"%@: \"%@\"\n" ,[[properties allKeys] objectAtIndex:i], [properties objectForKey:[[properties allKeys] objectAtIndex:i]]]];
+        [appendContents appendString:[NSString stringWithFormat:@"%@: \"%@\"\n" ,[[properties allKeys] objectAtIndex:i], [properties objectForKey:[[properties allKeys] objectAtIndex:i]]]];
 
     }
     
