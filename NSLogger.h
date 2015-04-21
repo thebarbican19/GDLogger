bd56559b8ec150dbf6d144abb9380e923a009166
NorthernSpark <joe@northernspark.co.uk>
2015-04-21 21:06:49 +0100

    NSLogger Version 1.0
    Log your app events to a backend log file

---
+++ NSLogger.h
@@ -0,0 +1,22 @@
+//
+//  NSLogger.h
+//  Video Downloader
+//
+//  Created by Joe Barbour on 21/04/2015.
+//  Copyright (c) 2015 NorthernSpark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NSLogger : NSObject
+
+-(void)log:(NSString *)title properties:(NSDictionary *)properties;
+
+-(NSURL *)logDirectory;
+-(NSString *)logPrint;
+-(NSData *)logData;
+
+@property (nonatomic) BOOL degbugger;
+
+@end
---
+++ NSLogger.m
@@ -0,0 +1,86 @@
+//
+//  NSLogger.m
+//  Video Downloader
+//
+//  Created by Joe Barbour on 21/04/2015.
+//  Copyright (c) 2015 NorthernSpark. All rights reserved.
+//
+
+#import "NSLogger.h"
+
+#define LOGGER_DIRECTORY [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"logger"]
+#define LOGGER_APP [[[NSBundle mainBundle] localizedInfoDictionary] objectForKey:@"CFBundleDisplayName"]
+#define LOGGER_BUNDLE [[NSBundle mainBundle] bundleIdentifier]
+#define LOGGER_DEVICE_NAME [UIDevice currentDevice].name
+#define LOGGER_LANGUAGE [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:[[NSLocale currentLocale] localeIdentifier]]
+#define LOGGER_DEVICE_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
+#define LOGGER_APP_FORMATTED [[LOGGER_APP stringByReplacingOccurrencesOfString:@" " withString:@"-"] lowercaseString]
+#define LOGGER_FILENAME [NSString stringWithFormat:@"%@-logger.txt" ,LOGGER_APP_FORMATTED]
+#define LOGGER_VERSION 1.0
+#define LOGGER_HEADER [NSString stringWithFormat:@"Created with NorthernSpark Logger (Version %f)\n (c)\nLog Created: %@\nApp Name: %@\nApp Bundle Identifyer: %@\nDevice: %@ (iOS %f)\nLocalization: %@\n\n\n" ,LOGGER_VERSION, [self formatDate], LOGGER_APP, LOGGER_BUNDLE, LOGGER_DEVICE_NAME, LOGGER_DEVICE_VERSION, LOGGER_LANGUAGE]
+
+@implementation NSLogger
+
+-(NSString *)formatDate {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setTimeZone:[NSTimeZone defaultTimeZone]];
+    [formatter setDateFormat:@"HH:mm:ss EEE dd/MM/yyyy"];
+    
+    return [formatter stringFromDate:[NSDate date]];
+    
+}
+
+-(void)log:(NSString *)title properties:(NSDictionary *)properties {
+    NSMutableString *appendContents = [[NSMutableString alloc] init];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:LOGGER_DIRECTORY]) {
+        [[NSFileManager defaultManager] createDirectoryAtPath:LOGGER_DIRECTORY withIntermediateDirectories:false attributes:nil error:nil];
+        
+        [appendContents appendString:LOGGER_HEADER];
+        
+        if (self.degbugger) NSLog(@"NSLogger file created: %@" ,LOGGER_FILENAME);
+        
+    }
+    
+    [appendContents appendString:[self logPrint]];
+    [appendContents appendString:[NSString stringWithFormat:@"\n\n****************************** %@ ******************************\n" ,[self formatDate]]];
+    [appendContents appendString:[NSString stringWithFormat:@"\nEVENT: \"%@\"\nPROPERTIES ¬ \n" ,title]];
+    
+    for (int i = 0; i < [[properties allKeys] count]; i++) {
+        if ([[properties objectForKey:[[properties allKeys] objectAtIndex:i]] isKindOfClass:[NSNumber class]]) NSAssert(1, @"properties dictionary cannot contain NSNumber objects.");
+        else [appendContents appendString:[NSString stringWithFormat:@"%@: \"%@\"\n" ,[[properties allKeys] objectAtIndex:i], [properties objectForKey:[[properties allKeys] objectAtIndex:i]]]];
+
+    }
+    
+    [appendContents appendString:@"\n"];
+
+    NSError *writingError;
+    if (![appendContents writeToURL:[self logDirectory] atomically:true encoding:NSUTF8StringEncoding error:&writingError]) {
+        if (self.degbugger) NSLog(@"NSLogger was not updated due to error: %@" ,writingError);
+
+    }
+    else {
+        if (self.degbugger) NSLog(@"NSLogger event \"%@\" added" ,title);
+        
+    }
+
+}
+
+-(NSURL *)logDirectory {
+    return [NSURL fileURLWithPath:[LOGGER_DIRECTORY stringByAppendingPathComponent:LOGGER_FILENAME]];
+    
+}
+
+-(NSString *)logPrint {
+    if ([[NSString stringWithContentsOfFile:[LOGGER_DIRECTORY stringByAppendingPathComponent:LOGGER_FILENAME] encoding:NSUTF8StringEncoding error:NULL] length] != 0)
+        return [NSString stringWithContentsOfFile:[LOGGER_DIRECTORY stringByAppendingPathComponent:LOGGER_FILENAME] encoding:NSUTF8StringEncoding error:NULL];
+    else return @"";
+    
+}
+
+-(NSData *)logData {
+    return [[NSFileManager defaultManager] contentsAtPath:[LOGGER_DIRECTORY stringByAppendingPathComponent:LOGGER_FILENAME]];
+    
+}
+
+
+@end
